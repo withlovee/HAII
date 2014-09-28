46b52be0745f672cd0748de394fc5879c6407d56
Nuttapon Pattanavijit <nuttt.p@gmail.com>
2014-09-28 20:21:29 +0700

    add data log for water

---
+++ rscripts/datalog.R
@@ -0,0 +1,53 @@
+source('db_connection.R')
+
+getWaterLevelData <- function(startDateTime, endDateTime) {
+  
+  con <- openDbConnection()
+  
+  startDateString <- strftime(startDateTime, "%Y-%m-%d");
+  endDateString   <- strftime(endDateTime, "%Y-%m-%d");
+  
+  startTimeString <- strftime(startDateTime, "%H:%M:%S");
+  endTimeString   <- strftime(endDateTime, "%H:%M:%S");
+    
+  data <- dbGetQuery(con,
+    paste("
+      SELECT 
+        data_log.code, 
+        data_log.date,
+        data_log.time,
+        data_log.water1,
+        tele_wl_detail.left_bank, 
+        tele_wl_detail.right_bank,
+        tele_wl_detail.ground_level
+    
+      FROM data_log
+    
+      INNER JOIN tele_wl_detail ON tele_wl_detail.code = data_log.code
+
+      WHERE data_log.date >= DATE '", startDateString ,"'
+      AND   data_log.time >= TIME '", startTimeString ,"'
+      AND   data_log.date <= DATE '", endDateString ,"'
+      AND   data_log.time <= TIME '", endTimeString ,"'
+  
+    ", sep="")
+  )
+  
+  closeDbConnection(con)
+  
+  return(data)
+  
+}
+
+get24HrWaterLevelData <- function (startDateTime = NA) {
+  
+  endDateTime <- Sys.time()
+  yesterday <- endDateTime - 24*60*60
+  
+  if(is.na(startDateTime) | yesterday > startDateTime) {
+    startDateTime <- yesterday
+  }
+  
+  getWaterLevelData(startDateTime, endDateTime)
+  
+}
