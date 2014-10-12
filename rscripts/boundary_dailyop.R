016fe9cd71b5d2056a179e4e9c5d3bd9bd2e94da
Nuttapon Pattanavijit <nuttt.p@gmail.com>
2014-10-12 23:00:53 +0700

    add daily op

---
+++ rscripts/boundary_dailyop.R
@@ -0,0 +1,86 @@
+source('datalog.R')
+source('boundary.R')
+
+dataType <- "WATER"
+problemType <- "BD"
+
+stationList <- getStationCodeList();
+
+
+cat("###########################################\n")
+cat(" Date Executed: ")
+cat(strftime(Sys.time()))
+cat("\n")
+cat("###########################################\n")
+
+flush.console()
+
+currentTime <- Sys.time()
+
+data <- NA
+
+for(station in stationList) {
+  
+  cat("\n==========[ STATION:")
+  cat(station)
+  cat(" ]==========\n")
+  flush.console()
+  
+  cat("Getting latest run time...\n")
+  flush.console()
+  
+  startDateTime <- getLatestBoundaryProblemRunTime(station, dataType, problemType)
+  
+  if(!is.na(startDateTime)) {
+    startDateTime <- startDateTime + 1
+  }
+  
+  str(startDateTime)
+  
+  cat("Loading 24hr data...\n")
+  flush.console()
+  
+  data <- get24HrWaterLevelData(station, startDateTime, currentTime)
+  # data <- get24HrWaterLevelData(station, endDateTime = as.POSIXct("2012-02-28"))
+  
+  str(data)
+  
+  if(nrow(data) > 0) {
+  
+  cat("Detecting BD Problem...\n")
+  flush.console()
+  
+  bdProblem <- searchBoundaryProblem(data)
+  
+  str(bdProblem)
+  
+  
+  if(is.data.frame(bdProblem)) {
+    
+  cat("Writing Logs...\n")
+  flush.console()
+  
+  
+  updateBoundaryProblem(bdProblem)
+  
+  } else {
+    cat("Problem not found...\n")
+  }
+  
+  cat("Update latest runtime...\n")
+  flush.console()
+  
+  end_datetime <- mapply(paste, data$date, data$time)
+  latestTime <- max(as.POSIXct(end_datetime))
+  
+  updateLatestBoundaryProblemRunTime(station, dataType, problemType, latestTime)
+  
+  } else {
+    cat("No data...\n")
+  }
+  
+  cat("DONE!...\n")
+  flush.console()
+  
+  
+}
