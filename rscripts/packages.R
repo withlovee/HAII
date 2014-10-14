5f4f688c473e7f65dff1c79d7cb5cbf672ae978d
Nuttapon Pattanavijit <nuttt.p@gmail.com>
2014-10-14 10:58:12 +0700

    edit package installer

--- rscripts/packages.R
+++ rscripts/packages.R
@@ -1,5 +1,11 @@
-install.packages('RPostgreSQL')
-install.packages('RUnit')
-install.packages('httr')
-install.packages('DBI')
-install.packages('jsonlite')
+
+packages <- c('RPostgreSQL', 'RUnit', 'httr', 'DBI')
+
+
+################################
+# DO NOT EDIT BELOW THIS LINE  #
+################################
+
+repos <- "http://cran.stat.nus.edu.sg/"
+
+install.packages(packages, repos=repos)
